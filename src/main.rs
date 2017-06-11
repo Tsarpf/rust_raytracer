b0b60d546abbce9d1049237d21e68e135964b984
Teemu Sarapisto <tsarpf@gmail.com>
2017-06-11 21:49:48 +0300

    Gradient werking

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,17 @@ fn main() {
 fn print_file(file: &mut BufWriter<File>) {
     let nx = 200;
     let ny = 100;
-    write!(file, "P3\n{} {}\n255", nx, ny);
+    write!(file, "P3\n{} {}\n255\n", nx, ny);
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r: f32 = (i as f32)/(nx as f32);
+            let g: f32  = (j as f32)/(ny as f32);
+            let b: f32  = 0.2;
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+            write!(file, "{} {} {}\n", ir, ig, ib).unwrap();
+        }
+    }
 }
