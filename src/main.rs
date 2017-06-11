3d5b34ed03bf27faee9f3d54c76d0d27ebb9d612
Teemu Sarapisto <tsarpf@gmail.com>
2017-06-11 12:32:15 +0300

    Add ppm file header printing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::prelude::*;
+use std::io::BufWriter;
+use std::fs::File;
+
 fn main() {
     println!("Hello, world!");
+    let mut file = BufWriter::new(File::create("test.ppm").unwrap());
+    print_file(&mut file);
+}
+
+fn print_file(file: &mut BufWriter<File>) {
+    let nx = 200;
+    let ny = 100;
+    write!(file, "P3\n{} {}\n255", nx, ny);
 }
