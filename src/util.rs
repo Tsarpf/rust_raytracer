cce89f3951f97d1f9592da6c578ee8e56befb8ab
Teemu Sarapisto <tsarpf@gmail.com>
2017-06-13 13:36:33 +0300

    Use better variable names in ray struct

--- src/util.rs
+++ src/util.rs
@@ -2,21 +2,24 @@ extern crate cgmath;
 use cgmath::Vector3;
 
 pub struct Ray {
-    A: Vector3<f32>,
-    B: Vector3<f32>,
+    origin: Vector3<f32>,
+    direction: Vector3<f32>,
 }
 
 impl Ray {
     pub fn new(a: Vector3<f32>, b: Vector3<f32>) -> Ray {
-        Ray { A: a, B: b }
+        Ray {
+            origin: a,
+            direction: b,
+        }
     }
-    pub fn point_at(&self, t: f32) {
-        self.A + t * self.B;
+    pub fn point_at(&self, t: f32) -> Vector3<f32> {
+        self.origin + t * self.direction
     }
     pub fn direction(&self) -> Vector3<f32> {
-        self.B
+        self.direction
     }
     pub fn origin(&self) -> Vector3<f32> {
-        self.A
+        self.origin
     }
 }
